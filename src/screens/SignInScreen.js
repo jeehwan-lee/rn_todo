51a156f3cf5f01f9e3f54211c9bf3253d22c792b
jeehwan-lee <tjtojan@naver.com>
2023-10-18 23:08:12 +0900

    image 컴포넌트 생성

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,24 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+function SignInScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../../assets/main.png")} style={styles.image} />
+      <Text>SignInScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default SignInScreen;
