0a38dbd71ded3e578bbeaab37f3d7622bc0163a5
jeehwan-lee <tjtojan@naver.com>
2023-10-19 22:30:34 +0900

    화면 전환 및 순서 설정

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 2. Keyboard API : dismiss()를 사용해서 키보드를 사라지게 할 수 있음
 
 3. onChange / onChangeText 차이는 event를 넘기는지 아니면 Text만 넘기는지
+
+4. createNativeStackNavigator(); 를 통해 화면들을 관리
+   4-1. 관리되는 화면들은 Props로 navigation을 받음
+   4-2. navigation.navigate 또는 navigation.push 을 통해 화면을 이동할 수 있음
+   4-3. push는 화면을 새로 계속 쌓지만 navigate 는 동일한 화면은 그대로 둠
+   4-4. 다만, navigate에 데이터를 전달할때 데이터가 바뀌면 화면을 쌓이지 않지만 랜더링은 다시 됨
+
+5. Navigator 에서 첫번째 Screen가 제일 먼저 뜨는 화면이지만,
+   initialRouteName으로 첫번째 화면을 정할 수 있음
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
 
-const ListScreen = () => {
+const ListScreen = ({ navigation }) => {
+  console.log("rendering ListScreen");
   return (
     <View style={StyleSheet.container}>
       <Text style={{ fontSize: 30 }}>List Screen</Text>
+      <Button title="push" onPress={() => navigation.push("List")} />
+      <Button title="navigate" onPress={() => navigation.navigate("List")} />
     </View>
   );
 };
