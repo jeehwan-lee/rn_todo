66f4a4b108e6e95247763d231ae2739f470110e5
jeehwan-lee <tjtojan@naver.com>
2023-10-19 21:55:38 +0900

    navigator 적용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { WHITE } from "./color";
+import AuthStack from "./navigations/Authstack";
 import SignInScreen from "./screens/SignInScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar style="dark" />
-      <SignInScreen />
-    </View>
+      <AuthStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: WHITE,
-  },
-});
