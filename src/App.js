e208623a7d42b63800632b4b6632618aa9e1bb3b
jeehwan-lee <tjtojan@naver.com>
2023-10-21 00:00:01 +0900

    context api 설정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { WHITE } from "./color";
+import UserContext from "./contexts/UserContext";
 import AuthStack from "./navigations/Authstack";
 import MainStack from "./navigations/MainStack";
 import SignInScreen from "./screens/SignInScreen";
@@ -10,13 +11,15 @@ import SignInScreen from "./screens/SignInScreen";
 export default function App() {
   const [user, setUser] = useState(null);
   return (
-    <NavigationContainer>
-      <StatusBar style="dark" />
-      {user ? (
-        <MainStack user={user} setUser={setUser} />
-      ) : (
-        <AuthStack user={user} setUser={setUser} />
-      )}
-    </NavigationContainer>
+    <UserContext.Provider value={"beomjun"}>
+      <NavigationContainer>
+        <StatusBar style="dark" />
+        {user ? (
+          <MainStack user={user} setUser={setUser} />
+        ) : (
+          <AuthStack user={user} setUser={setUser} />
+        )}
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 }
